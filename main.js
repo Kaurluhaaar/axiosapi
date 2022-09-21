bd70691164761b842a3f7ff2ccbf4ff1e5a0b44b
kaurluhaaar <kaur.luhaaar@ametikool.ee>
2022-09-21 11:40:35 +0300

    got api to upload on page

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-import { API } from "./config/API.js";
+import { API } from './config/API';
 
 try {
-    API.get('statistics').then(response=> {console.log(response)})
-} catch(error) {
-    throw new Error(error.response.message)
+    await API.get('countries')
+    .then(response => {
+        const allCountries = response.data.response;
+        console.log(allCountries)
+        allCountries.forEach(country => {
+            var tag = document.createElement("option");
+            tag.innerHTML = country;
+            var element = document.getElementById("countries")
+            element.appendChild(tag);
+        });
+    })
+} catch (error) {
+    console.error(error);
 }
+const selector = document.getElementById('countries');
+selector.addEventListener('input', (e) => {
+    API.get('/statistics', {params: {country: e.target.value}}) 
+    .then(response => {
+        console.log(response.data.response[0])
+    })
+    console.log(e.target.value)
+})
