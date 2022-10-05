8cefb8bf2745b5de9e96ec12df0a4ee2bcec952a
kaurluhaaar <kaur.luhaaar@ametikool.ee>
2022-10-05 14:13:23 +0300

    show the info of api when selected country

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ selector.addEventListener('input', (e) => {
     API.get('/statistics', {params: {country: e.target.value}}) 
     .then(response => {
         console.log(response.data.response[0])
+        let infoDiv = document.getElementById('infoDiv')
+            infoDiv.innerHTML = '<p> Population: ' + response.data.response[0].population + '</p>' + '<p> Cases: ' +
+            response.data.response[0].cases.active + '</p>'
     })
     console.log(e.target.value)
 })
